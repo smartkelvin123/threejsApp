9dfc6ca65592efea6d40ebed2fa3bdc0f31fb379
unknown <smartkelvin66@gmail.com>
2023-09-10 09:26:34 +0100

    box

--- App.js
+++ App.js
@@ -1,54 +1,39 @@
-import { Canvas } from "@react-three/fiber/native";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber/native";
+
+function Box(props) {
+  const meshRef = useRef(null);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += delta;
+  });
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
 
 export default function App() {
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
-
-      <mesh position={[-1, -1, 0]}>
-        <torusKnotGeometry />
-        <meshStandardMaterial color={"green"} />
-      </mesh>
+      <Box position={[0, -2, 0]} />
+      <Box position={[0, -1, 0]} />
+      <Box position={[0, 0, 0]} />
+      <Box position={[0, 1, 0]} />
+      <Box position={[0, 2, 0]} />
     </Canvas>
   );
 }
-
-// import React, { useRef, useState } from "react";
-// import { Canvas, useFrame } from "@react-three/fiber/native";
-
-// function Box(props) {
-//   const meshRef = useRef(null);
-//   const [hovered, setHover] = useState(false);
-//   const [active, setActive] = useState(false);
-//   useFrame((state, delta) => (meshRef.current.rotation.x += 0.01));
-//   return (
-//     <mesh
-//       {...props}
-//       ref={meshRef}
-//       scale={active ? 1.5 : 1}
-//       onClick={(event) => setActive(!active)}
-//       onPointerOver={(event) => setHover(true)}
-//       onPointerOut={(event) => setHover(false)}
-//     >
-//       <boxGeometry args={[1, 1, 1]} />
-//       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-//     </mesh>
-//   );
-// }
-
-// export default function App() {
-//   return (
-//     <Canvas>
-//       <ambientLight />
-//       <pointLight position={[10, 10, 10]} />
-//       <Box position={[-1.2, 0, 0]} />
-//       <Box position={[1.2, 0, 0]} />
-//     </Canvas>
-//   );
-// }
