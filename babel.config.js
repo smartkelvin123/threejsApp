59a47de1f35003776229d80064072f983b1589d2
unknown <smartkelvin66@gmail.com>
2023-09-10 08:28:15 +0100

    threejs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
   };
 };
+
+// module.exports = {
+//   resolver: {
+//     sourceExts: ["js", "jsx", "json", "ts", "tsx", "cjs"],
+//     assetExts: ["glb", "gltf", "png", "jpg"],
+//   },
+// };
